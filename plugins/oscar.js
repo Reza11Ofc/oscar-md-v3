01a0b28c95540c5f60dd39e2cb4e3dabc4f0cba4
Reza11Ofc <113843050+Reza11Ofc@users.noreply.github.com>
2022-09-20 03:42:23 +0700

    Update oscar.js

--- plugins/oscar.js
+++ plugins/oscar.js
@@ -37,14 +37,14 @@ let jarot = `
            hydratedButtons: [{
              urlButton: {
                displayText: '𝙂𝙧𝙤𝙪𝙥 𝘽𝙤𝙩🎮',
-               url: 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+               url: 'https://chat.whatsapp.com/Htbopc53nFD3puFb3LfMe1'
              }
 
            },
                 {
                urlButton: {
                displayText: '𝙈𝙮 𝙒𝙚𝙗🔮',
-               url: 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+               url: 'https://youtube.com/channel/UCoUIE95oCiyo_Ps0l9rhcdw'
              }
 
            },
