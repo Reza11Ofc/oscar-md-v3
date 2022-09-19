b0d6259fb2d386e011eb02bcb0b93b4a1aca5d51
Reza11Ofc <113843050+Reza11Ofc@users.noreply.github.com>
2022-09-20 05:14:27 +0700

    Update oscarhi.js

--- plugins/oscarhi.js
+++ plugins/oscarhi.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let hallo = fs.readFileSync('./mp3/WhatsApp-Audio-2021-03-02-at-20.52.32-_2_.opus') 
+let hallo = fs.readFileSync('mp3/haigabut.opus') 
 conn.sendFile(m.chat, hallo, '', '', m, true)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hi bot|tes|tes|hy|halo|hallo|helo|hello|hy jarot|halo jarot|bot|helo bot|hello bot)$/i
+handler.customPrefix = /^(hi|hii|hiii|hi bot|tes|tes|hy|halo|hallo|helo|hello|hy eza|halo eza|bot|helo bot|hello bot)$/i
 handler.command = new RegExp
 
 handler.limit = true
