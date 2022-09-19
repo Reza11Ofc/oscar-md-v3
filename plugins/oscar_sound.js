22ccf16cc11eff18935e938c8fc11697b3598569
Reza11Ofc <113843050+Reza11Ofc@users.noreply.github.com>
2022-09-20 03:39:21 +0700

    Update oscar_sound.js

--- plugins/oscar_sound.js
+++ plugins/oscar_sound.js
@@ -4,7 +4,7 @@ let jarot = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.80.opus')
 conn.sendFile(m.chat, jarot, '', '', m, true)
 }
 
-handler.customPrefix = /^(jarot)$/i
+handler.customPrefix = /^(Eza|EzaBot|eja|ejabot|EzaBotz-|EzaBots-MD|EzaBots|Ezabotz|MD)$/i
 handler.command = new RegExp
 
 handler.limit = true
